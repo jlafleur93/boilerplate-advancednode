c440d8910ac5c1eb022c705d2a8414334e713f27
joelwowz <josephlafleur@hotmail.com>
2021-08-17 01:10:34 -0600

    finished challenge

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 "use strict";
-require("dotenv").config();
+require("dotenv").config({ path: "../sample.env" });
 const express = require("express");
 const myDB = require("./connection");
+const passport = require("passport");
+const session = require("express-session");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
@@ -11,6 +13,17 @@ app.set("view engine", "pug");
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "42",
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  }),
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.route("/").get((req, res) => {
   const ObjValues = {
