3a6705fdb2cae3c4e59d57e1647db684b50291b0
joelwowz <josephlafleur@hotmail.com>
2021-08-17 03:34:30 -0600

    finished fcc challenge

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 
 const express = require("express");
 const myDB = require("./connection");
-const passport = require("passport");
-const session = require("express-session");
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
 
 const app = express();
-
-fccTesting(app); //For FCC testing purposes
 app.set("view engine", "pug");
+
+fccTesting(app); // For fCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -23,26 +23,40 @@ app.use(
     cookie: { secure: false },
   }),
 );
+
 app.use(passport.initialize());
 app.use(passport.session());
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
+
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  // Be sure to change the title
+  app.route("/").get((req, res) => {
+    // Change the response to render the Pug template
+    const ObjValues = {
+      title: "Connected to Database",
+      message: "Please login",
+    };
+    res.render("../views/pug", ObjValues);
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
   });
 });
+// app.listen out here...
 
-app.route("/").get((req, res) => {
-  const ObjValues = {
-    title: "Hello",
-    message: "Please login",
-  };
-  res.render("../views/pug", ObjValues);
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + process.env.PORT);
 });
