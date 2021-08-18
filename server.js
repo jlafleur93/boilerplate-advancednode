f384ce2917a2448baf640d011121d40dc3c72963
joelwowz <josephlafleur@hotmail.com>
2021-08-17 22:10:48 -0600

    finished fcc challenge

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ myDB(async (client) => {
       message: "Please login",
       showLogin: true,
     };
-    res.render("pug/index.pug", ObjValues);
+    res.render("pug/index", ObjValues);
   });
 
   // Serialization and deserialization here...
@@ -81,13 +81,22 @@ app
   .post(
     passport.authenticate("local", { failureRedirect: "/" }),
     (req, res) => {
-      res.redirect("/profile ");
+      res.redirect("pug/profile");
     },
   );
 
-app.route("/profile").get((req, res) => {
-  res.render(process.cwd() + "/views/pug/profile");
+app.route("/profile").get(ensureAuthenticated, (req, res) => {
+  res.render(process.cwd() + "/views/pug/profile", {
+    username: req.user.username,
+  });
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
